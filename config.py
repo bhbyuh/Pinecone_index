439c3fe1b819c9aaea9582364c7b8c58646674c5
Muaaz <105689809+bhbyuh@users.noreply.github.com>
2024-06-15 04:22:24 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+api_key=''
+index_name='firstindex'
+dimensions=384
+file_path=r'C:\Users\muaaz\OneDrive\Desktop\Pineconne\sensors-22-08281-v3.pdf'
+chunk_size=500
+chunk_overlap=50
+embed_model="BAAI/bge-small-en-v1.5"
