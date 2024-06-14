57eb5bc3cfbc743d64f6d52f4beef11853041a83
Muaaz <105689809+bhbyuh@users.noreply.github.com>
2024-06-15 04:21:40 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-api_key='0303a663-aa0b-4a2f-ac7e-de829ee67b3e'
-index_name='firstindex'
-dimensions=384
-file_path=r'C:\Users\muaaz\OneDrive\Desktop\Pineconne\sensors-22-08281-v3.pdf'
-chunk_size=500
-chunk_overlap=50
-embed_model="BAAI/bge-small-en-v1.5"
